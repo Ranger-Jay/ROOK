36c2f62d11a8c52f256144fbad52503d0d104cac
Jay <careerforjay@gmail.com>
2026-08-24 20:20:58 -0700

    feat: scaffold Citadel Watch command center shell

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles/tokens.css'
+import './styles/typography.css'
+import './styles/global.css'
+
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('ROOK root element was not found.')
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
